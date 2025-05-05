51392a53a437e980bb4571f98474dbe4b3d3b7b6
harimurari27 <mallelaharimurari@gmail.com>
2025-05-06 02:10:56 +0530

    Initial commit

---
+++ stopwatch/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './index.css';
+import Stopwatch from './Component/Stopwatch';
+
+function App() {
+  return (
+    <>
+    <Stopwatch/>
+    </>
+  );     
+}
+
+export default App;
